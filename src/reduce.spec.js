a5cac32b715ca9b37246bbe7a904ce604e7e9dc7
geetanshkalra20 <geetansh.kalra@thoughtworks.com>
2021-11-08 00:51:52 +0530

    [Geetansh] Add. Logic for reduce where it should return zabc when an array is given a,b,c with initial value specified z

--- src/reduce.spec.js
+++ src/reduce.spec.js
@@ -5,5 +5,14 @@ describe('Reduce', () => {
     it('Should return [] if length of array is zero', () => {
         expect(reduce([], (x,y)=>x+y)).toEqual([]);
     });
+
+    it('Should return initial value 10 if length of array is zero', () => {
+        expect(reduce([], (x,y)=>x+y, 10)).toEqual(10);
+    });
+
+    it('Should return abc from an array [a,b,c]', () => {
+        expect(reduce(['a','b','c'], (x,y)=>x+y, 'z')).toEqual("zabc");
+    });
+
 })
   
